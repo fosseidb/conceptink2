b92989ccf23d970b6660def105d326dfa62f007b
Alexander Fosseidbraten <alex.fosseidbraten@gmail.com>
2014-02-28 00:11:51 +0000

    css for articles and artists

--- routes/article.js
+++ routes/article.js
@@ -1,5 +1,20 @@
+//json feed
+var data = require('../newsfeed.json');
+
 exports.viewArticle = function(req, res){
 	// controller code goes here
-	console.log("The project name is Article");
-	res.render('article');
+	var articles = data.articles;
+	var target = req.params.articleid;
+	var index;
+
+	for (var i=0; i< articles.length; i++)
+		{ 
+			if (articles[i].id == target)
+			index = i;
+	}
+	
+	// controller code goes here
+	console.log("The project name is article"+ index);
+	console.log(articles[index]);
+	res.render('article', articles[index]);
 };
